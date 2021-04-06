ed85afc3e921589a5ded106499f2da10fca37eee
Won <jungwon1423@gamil.com>
2021-04-06 20:04:14 +0900

    Add: SearchView 생성 Modify: SearchView 수정 Add: unit test Modify: unit test 성공

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from product.views import ProductFilterView
+from product.views import ProductFilterView, SearchView
 
 urlpatterns = [
-    path('', ProductFilterView.as_view())
+    path('', ProductFilterView.as_view()),
+    path('/search', SearchView.as_view())
 ]
